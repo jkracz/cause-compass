2b31596208ea6a10c19342ffc774b2596a15495e
Joe Kracz <joseph.kracz@gmail.com>
2024-06-15 15:49:38 -0700

    updated script with some logs for troubleshooting secrets in action file

--- apps/parsley/src/scripts/googleSearchOrgs.ts
+++ apps/parsley/src/scripts/googleSearchOrgs.ts
@@ -4,6 +4,9 @@ import { googleSearch } from "../utils/googleSearch";
 import "dotenv/config";
 
 const googleSearchOrgs = async () => {
+    console.log("GOOGLE_SEARCH_API_KEY:", process.env.GOOGLE_SEARCH_API_KEY);
+    console.log("MONGO_CONNECTION_STRING:", process.env.MONGO_CONNECTION_STRING);
+    console.log("SEARCH_ENGINE_ID:", process.env.SEARCH_ENGINE_ID);
     const orgs: TaxExemptOrganization[] = await getTaxExemptOrgsToSearch();
     for (const org of orgs) {
         const searchQuery: string = `${org.name} ${org.city} ${org.state}`;
