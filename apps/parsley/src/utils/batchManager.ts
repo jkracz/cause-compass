98431334ea7b3a350a51d3eb9a57e4e86004d465
Joe Kracz <22948854+jkracz@users.noreply.github.com>
2025-05-11 18:15:17 -0700

    fix: lowering batch size

--- apps/parsley/src/utils/batchManager.ts
+++ apps/parsley/src/utils/batchManager.ts
@@ -9,7 +9,7 @@ import { writeConfirmationFile } from "../scripts/generateBatchConfirmationFile"
 import { processBatchResponseFile } from "./batchResponseProcessor";
 import { insertBatchJob, updateBatchJob, findActiveBatchJob } from "../db/mongo";
 
-const DEFAULT_BATCH_SIZE = 200;
+const DEFAULT_BATCH_SIZE = 100;
 export class BatchManager {
     private openai: OpenAI;
     private tempDir: string;
