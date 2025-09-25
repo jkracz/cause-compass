c78a74b9b579187ff5c7f3b07a286f2abc83d92e
Joe Kracz <22948854+jkracz@users.noreply.github.com>
2025-09-25 07:40:11 -0700

    spike: installing and demoing ai sdk; will repurpose it for semantic search

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,33 @@
+import { openai } from "@ai-sdk/openai";
+import { streamText, UIMessage, convertToModelMessages, tool } from "ai";
+import { z } from "zod";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: openai("gpt-5-nano"),
+    messages: convertToModelMessages(messages),
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location (fahrenheit)",
+        inputSchema: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => {
+          console.log("location", location);
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
+  });
+
+  return result.toUIMessageStreamResponse();
+}
